371001110bb50c9dd3d59a78d9537ec3b43048b4
leeq <12691282@qq.com>
2018-12-03 20:44:03 +0800

    log update

--- titans-project/eureka-service/src/main/java/com/lion/eureka/EurekaApplication.java
+++ titans-project/eureka-service/src/main/java/com/lion/eureka/EurekaApplication.java
@@ -5,11 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-import lombok.extern.java.Log;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
 @EnableEurekaServer
-@Log
+@Slf4j
 public class EurekaApplication {
 
 	public static void main(String[] args) throws Exception {
