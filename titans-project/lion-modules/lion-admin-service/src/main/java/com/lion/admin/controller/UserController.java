26fc3b5dda72422d5cb3e3fde94bcb80aca067ad
leeq <12691282@qq.com>
2018-12-04 21:49:06 +0800

    modify

--- titans-project/lion-modules/lion-admin-service/src/main/java/com/lion/admin/controller/UserController.java
+++ titans-project/lion-modules/lion-admin-service/src/main/java/com/lion/admin/controller/UserController.java
@@ -2,26 +2,15 @@ package com.lion.admin.controller;
 
  
 
-import com.baomidou.mybatisplus.plugins.Page;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.lion.admin.model.bean.UserBean;
 import com.lion.admin.service.SysUserService;
 import com.lion.common.core.base.BaseController;
 import com.lion.common.tools.ResultInfo;
-import com.luhuiguo.fastdfs.domain.StorePath;
-import com.luhuiguo.fastdfs.service.FastFileStorageClient;
-import com.xiaoleilu.hutool.io.FileUtil;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author lion
