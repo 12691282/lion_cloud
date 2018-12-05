73c361448f6f0aec481dd7e4a550fe47e471a051
lion007 <12691282@qq.com>
2018-12-05 17:58:50 +0800

    create 17：58

---
+++ titans-project/lion-modules/lion-admin-service/src/main/java/com/lion/admin/common/event/RouteConfigInitEvent.java
@@ -0,0 +1,17 @@
+package com.lion.admin.common.event;
+
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 动态路由加载事件
+ * @author lion
+ *
+ */
+
+@Slf4j
+@Component
+public class RouteConfigInitEvent {
+
+}
