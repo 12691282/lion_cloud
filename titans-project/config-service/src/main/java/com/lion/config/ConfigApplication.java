9658ce7ae10dbb8345e057a1e82b2b1b24329e6b
leeq <12691282@qq.com>
2018-12-06 21:04:49 +0800

    fix bus refresh

--- titans-project/config-service/src/main/java/com/lion/config/ConfigApplication.java
+++ titans-project/config-service/src/main/java/com/lion/config/ConfigApplication.java
@@ -1,15 +1,12 @@
 package com.lion.config;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-
+@EnableDiscoveryClient
 @EnableConfigServer
-@EnableAutoConfiguration
-@EnableEurekaClient
 @SpringBootApplication
 public class ConfigApplication {
 
