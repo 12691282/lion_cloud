2aa7c2411b5f987ac0762c1efa7a0b4e4aab90c5
leeq <12691282@qq.com>
2018-12-16 20:33:38 +0800

    test edit

--- titans-project/web-service/src/main/java/com/lion/web/WebApplication.java
+++ titans-project/web-service/src/main/java/com/lion/web/WebApplication.java
@@ -1,5 +1,7 @@
 package com.lion.web;
 
+import com.alibaba.fastjson.JSON;
+import org.apache.http.message.BasicHeader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
@@ -8,7 +10,14 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @EnableFeignClients
@@ -17,22 +26,32 @@ import org.springframework.web.client.RestTemplate;
 @EnableCircuitBreaker
 public class WebApplication {
 
-  
-	public static void main(String[] args) {
-//		SpringApplication.run(WebApplication.class, args);
-//      String url = "http://127.0.0.1:9999/auth/oauth/token";
-      String url = "http://192.168.2.103:53620/bus/refresh";
-      RestTemplate restTemplate = new RestTemplate();
-      HttpHeaders headers = new HttpHeaders();
-      MediaType type = MediaType.parseMediaType("application/json; charset=UTF-8");
-      headers.setContentType(type);
-      headers.add("Accept", MediaType.APPLICATION_JSON.toString());
 
-      HttpEntity<String> formEntity = new HttpEntity<String>("", headers);
-      String result = restTemplate.postForObject(url, formEntity, String.class);
+	public static void main(String[] args) throws UnsupportedEncodingException {
+//		SpringApplication.run(WebApplication.class, args);
+//      String url = "http://192.168.2.103:50410/bus/refresh";
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("User-Agent","Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.16 Safari/537.36");
+        headers.add("Accept-Encoding", "gzip,deflate");
+        headers.add("Accept-Language", "zh-CN");
+        headers.add("Connection", "Keep-Alive");
+        String url = "http://127.0.0.1:9999/auth/oauth/token";
+        MultiValueMap<String, Object> paramMap = new LinkedMultiValueMap();
+        paramMap.add("username",  "admin");
+        paramMap.add("password",  "lBTqrKS0kZixOFXeZ0HRng==");
+        paramMap.add("randomStr", "19201544884633838");
+        paramMap.add("code","1111");
+        paramMap.add("grant_type","password");
+        paramMap.add("scope","server");
+        paramMap.add("client_id","lion");
+        paramMap.add("client_secret","lion");
+        HttpEntity<MultiValueMap<String, Object>> httpEntity = new HttpEntity(paramMap,headers);
+        RestTemplate restTemplate = new RestTemplate();
+
+          ResponseEntity<String> result = restTemplate.postForEntity(url, httpEntity, String.class);
 
 //      SpringApplication.run(PigSsoClientDemoApplication.class, args);
-     System.out.println( result);
+        System.out.println( result);
 	
 	}
 	
