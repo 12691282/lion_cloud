f729cd1cbbc6a886a3297f2c3345a0a19c3e3d46
lion007 <12691282@qq.com>
2018-12-17 18:00:33 +0800

    test modify

--- titans-project/web-service/src/main/java/com/lion/web/WebApplication.java
+++ titans-project/web-service/src/main/java/com/lion/web/WebApplication.java
@@ -1,6 +1,7 @@
 package com.lion.web;
 
 import com.alibaba.fastjson.JSON;
+import com.lion.common.tools.Base64Tool;
 import org.apache.http.message.BasicHeader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -29,13 +30,15 @@ public class WebApplication {
 
 	public static void main(String[] args) throws UnsupportedEncodingException {
 //		SpringApplication.run(WebApplication.class, args);
-//      String url = "http://192.168.2.103:50410/bus/refresh";
+
         HttpHeaders headers = new HttpHeaders();
         headers.add("User-Agent","Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.16 Safari/537.36");
         headers.add("Accept-Encoding", "gzip,deflate");
         headers.add("Accept-Language", "zh-CN");
         headers.add("Connection", "Keep-Alive");
-        String url = "http://127.0.0.1:9999/auth/oauth/token";
+        String code = "lion:lion";
+        String auth = "Basic "+Base64Tool.encode(code);
+        headers.add("Authorization", auth);
         MultiValueMap<String, Object> paramMap = new LinkedMultiValueMap();
         paramMap.add("username",  "admin");
         paramMap.add("password",  "lBTqrKS0kZixOFXeZ0HRng==");
@@ -43,8 +46,10 @@ public class WebApplication {
         paramMap.add("code","1111");
         paramMap.add("grant_type","password");
         paramMap.add("scope","server");
-        paramMap.add("client_id","lion");
-        paramMap.add("client_secret","lion");
+
+//      String url = "http://127.0.0.1:49732/bus/refresh";
+        String url = "http://127.0.0.1:9999/auth/oauth/token";
+
         HttpEntity<MultiValueMap<String, Object>> httpEntity = new HttpEntity(paramMap,headers);
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> result = restTemplate.postForEntity(url, httpEntity, String.class);
