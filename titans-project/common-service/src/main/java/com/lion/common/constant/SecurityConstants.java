ea078b730d9e4a2b0cfca30ec2fb95ed3d6c2458
lion007 <12691282@qq.com>
2018-12-25 11:05:09 +0800

    1105

--- titans-project/common-service/src/main/java/com/lion/common/constant/SecurityConstants.java
+++ titans-project/common-service/src/main/java/com/lion/common/constant/SecurityConstants.java
@@ -2,11 +2,11 @@ package com.lion.common.constant;
 
 public class SecurityConstants {
     /**
-     * 用户信息头1
+     * 用户信息头2
      */
     public static final String USER_HEADER = "x-user-header";
     /**
-     * 角色信息头1
+     * 角色信息头2
      */
     public static final String ROLE_HEADER = "x-role-header";
 
