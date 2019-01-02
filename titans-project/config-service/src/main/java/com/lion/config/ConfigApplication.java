b2402c13feedae97ceff93acf22ef97150fe29e0
apple <12691282@qq.com>
2019-01-02 16:53:09 +0800

    log output

--- titans-project/config-service/src/main/java/com/lion/config/ConfigApplication.java
+++ titans-project/config-service/src/main/java/com/lion/config/ConfigApplication.java
@@ -1,5 +1,6 @@
 package com.lion.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,10 +9,12 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
+@Slf4j
 public class ConfigApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigApplication.class, args);
+		log.info("config service is ready");
 	}
 	
 }
